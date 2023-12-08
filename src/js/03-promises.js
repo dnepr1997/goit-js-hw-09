e5363b62929d0c55871aefbf09993996550e3646
Olexander <dneprovsky1997@icloud.com>
2023-12-08 11:53:50 +0100

    finish last

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,35 +1,40 @@
-
 import Notiflix from 'notiflix';
+const form = document.querySelector('.form')
+const btn = document.querySelector('button')
 
-const form = document.querySelector(".form");
+form.addEventListener('submit', hendleForn)
 
-form.addEventListener("submit", handleSubmit);
-
-function handleSubmit(event) {
-    event.preventDefault();
-
-    const { delay, step, amount } = event.currentTarget.elements;
-
-    setTimeout(() => {
-        for(let i = 1; i <= +amount.value; i++) {
-            createPromise(i, (step.value * i))
-                .then(data => console.log(data))
-                .catch(error => console.log(error))
-        }
-    }, +delay.value)
+function hendleForn(event) {
+  event.preventDefault()
+    const { delay, step, amount } = event.currentTarget.elements
+    let newDelay = +(delay.value)
+    let newStep = +(step.value)
+    let newAmount = +(amount.value)
+    
+    
+    for (let i = 1; i < newAmount; i++) {
+        
+       createPromise(i, newDelay) 
+        .then(({ position, delay }) => {
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+        newDelay += newStep
+    }
 }
-
 function createPromise(position, delay) {
     return new Promise((resolve, reject) => {
+        const shouldResolve = Math.random() > 0.3;
         setTimeout(() => {
-            const shouldResolve = Math.random() > 0.3;
             if (shouldResolve) {
-                // Fulfill
-                resolve(Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`))
-              } else {
-                reject(Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`))
-                // Reject
-              }
+                resolve({ position, delay })
+  } else {
+                reject({ position, delay })
+  }
         }, delay)
     })
 }
+
+
